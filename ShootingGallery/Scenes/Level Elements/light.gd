3fec0de948f0289af8936290d2ae8a4c2abe87f1
MmmGarlicFingers <120672054+MmmGarlicFingers@users.noreply.github.com>
2024-02-24 15:59:26 -0400

    Added the funky lights

--- ShootingGallery/Scenes/Level Elements/light.gd
+++ ShootingGallery/Scenes/Level Elements/light.gd
@@ -2,16 +2,22 @@ extends Control
 
 @export var shoot: bool = false
 var image
+@onready var light = $PointLight2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	image = $TextureRect
+	
+	
 
 func turn_on():
+	light.enabled = true
+	image.texture = preload("res://assets/light_on.png")
 	if shoot:
-		image.texture = load("./assets/light_shoot.png")
+		light.color = Color.GREEN
 	else:
-		image.texture = load("./assets/light_no_shoot.png")
+		light.color = Color.RED
 
 func turn_off():
-	image.texture = load("./assets/light_off.png")
+	image.texture = preload("res://assets/light_off.png")
+	light.enabled = false
